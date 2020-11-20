a5d2e221306aa999bd2312dff86eda6d9a7f5fa0
Javier Omar <41528232+javix70@users.noreply.github.com>
2020-11-19 21:44:29 -0300

    initial commit

---
+++ imc.py
@@ -0,0 +1,22 @@
+
+# -*- coding: utf-8 -*-
+
+lista_personas = []
+
+def registrarPersona(nombre, run, correo, password):
+    lista_personas.append([nombre, run, correo, password])
+
+def calcularIMC():
+    pass
+
+def mostrarEstadoNutricional():
+    pass 
+
+def validarCorreo():
+    pass
+
+def calcularContraseña():
+    pass
+
+if __name__ == "__main__":
+    pass
