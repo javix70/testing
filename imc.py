296ceecd641b75e0fb0773f465c18a73c1f19e23
alexissaez <alexis.saez@simplit-solutions.com>
2020-11-19 23:33:33 -0300

    se avanza en la tarea
    Co-authored-by: Javier Omar <javier17omar@gmail.com>

--- imc.py
+++ imc.py
@@ -1,22 +1,20 @@
-
 # -*- coding: utf-8 -*-
+import re
 
 lista_personas = []
 
 def registrarPersona(nombre, run, correo, password):
     lista_personas.append([nombre, run, correo, password])
 
-def calcularIMC():
-    pass
+def calcularIMC(peso,altura):
+    return peso/altura**2
 
 def mostrarEstadoNutricional():
-    pass 
-
-def validarCorreo():
     pass
 
-def calcularContraseña():
-    pass
+def validarCorreo(correo):
+    pattern = r'^\w+[\w|\d]*@\w+[\w|\d]*.\w{2,3}$'
+    return re.match(pattern, correo)
 
-if __name__ == "__main__":
-    pass
+def calcularContrasena(correo,rut):
+    return  correo.split('@')[0] + rut[:4]
---
+++ test_imc.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from imc import registrarPersona, lista_personas, calcularIMC, validarCorreo, calcularContrasena
+class TestImc(unittest.TestCase):
+
+    def setUp(self):
+      lista_personas = []
+
+    def test_guardar_en_lista_datos_de_persona(self):
+      nombre = "javier",
+      run = "11112-9",
+      correo = "yo@mi.cl",
+      password = "yo1111"
+      resultado_esperado = [[nombre, run, correo, password]]
+      registrarPersona(nombre, run, correo, password)
+      self.assertEqual(lista_personas, resultado_esperado)
+
+    def test_calcular_imc(self):
+      peso = 80
+      altura = 1.80
+      resultado_esperado = 24.69
+      resultado_imc = calcularIMC(peso, altura)
+      self.assertEqual(resultado_imc, resultado_esperado)
+
+    def test_validar_correo_correcto(self):
+      correo = "yo@mi.cl"
+      resultado = validarCorreo(correo)
+      self.assertTrue(resultado)
+
+    def test_validar_correo_incorrecto(self):
+      correo = "no.es.un.correo.valido"
+      resultado = validarCorreo(correo)
+      self.assertFalse(resultado)
+
+    def test_calcular_contrasena(self):
+      rut = '19341549-0'
+      correo = "yo@mi.cl"
+      resultado = calcularContrasena(correo,rut)
+      resultado_esperado = 'yo1934'
+      self.assertEqual(resultado, resultado_esperado)
